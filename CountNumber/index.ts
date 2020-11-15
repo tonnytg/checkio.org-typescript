e1986ab8e3df8a6c8ce869a98eae350832f941f1
Antonio Thomacelli <tonnytg@gmail.com>
2020-11-15 08:00:51 -0300

    solved count letters

--- CountNumber/index.ts
+++ CountNumber/index.ts
@@ -1,13 +1,38 @@
 // https://js.checkio.org/mission/count-digits/solve/
-// https://js.checkio.org/user/tonnytg/badges/solver/
+// https://js.checkio.org/mission/count-digits/publications/tonnytg/js-node/first/
 
-function countDigits(text: any ):number {
-        let Total: number = 0;
-        for ( let element in text ) {
-            Number(text[element]) ? Total = Total + 1: null ;
-        };
-        return Total
+import assert from "assert";
+
+function countDigits(text: string): number {
+    let x: number = 0;
+    for (let y = 0; y <= text.length; y++){
+        if ( text[y] != " ") {
+            console.log(text[y]);
+            
+            console.log(typeof(text[y]))
+            // que engraçado para Number 0 não conta como numbero, o sistema transforma em false
+            if ( text[y] == "0" ) {
+                x = x + 1
+            } else {
+                if ( Number(text[y]) ) {
+                    x = x + 1
+                }
+            }
+        }
+    };
+    return x;
 };
 
-let Valor = countDigits("5 plus 6 is")
-console.log(Valor + typeof(Valor));
+
+console.log('Example:');
+console.log(countDigits('hi'));
+
+// These "asserts" are used for self-checking
+assert.equal(countDigits('hi'), 0);
+assert.equal(countDigits('who is 1st here'), 1);
+assert.equal(countDigits('my numbers is 2'), 1);
+assert.equal(countDigits('This picture is an oil on canvas '
+ + 'painting by Danish artist Anna '
+ + 'Petersen between 1845 and 1910 year'), 8);
+assert.equal(countDigits('5 plus 6 is'), 2);
+assert.equal(countDigits(''), 0);
