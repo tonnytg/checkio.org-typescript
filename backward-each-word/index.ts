ca8466c3d46ebbb58156cb270a334d2d41ad55ab
Antonio Thomacelli <tonnytg@gmail.com>
2021-04-02 15:05:56 -0300

    quest: new changelle accept

---
+++ backward-each-word/index.ts
@@ -0,0 +1,18 @@
+import assert from "assert";
+
+function backwardStringByWord(text: string): string {
+    // your code here
+    return undefined;
+}
+
+console.log('Example:');
+console.log(backwardStringByWord(''));
+
+// These "asserts" are used for self-checking
+assert.equal(backwardStringByWord(''), '');
+assert.equal(backwardStringByWord('world'), 'dlrow');
+assert.equal(backwardStringByWord('hello world'), 'olleh dlrow');
+assert.equal(backwardStringByWord('hello   world'), 'olleh   dlrow');
+assert.equal(backwardStringByWord('welcome to a game'), 'emoclew ot a emag');
+
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
